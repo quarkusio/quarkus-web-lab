28f6b00ef6f0de99b26bfdcced1332c5deeb9291
Phillip Kruger <phillip.kruger@gmail.com>
2024-05-09 12:26:57 +1000

    Added basic backend for comments
    Signed-off-by: Phillip Kruger <phillip.kruger@gmail.com>

---
+++ comments/src/main/java/web/workshop/comments/Comment.java
@@ -0,0 +1,22 @@
+package web.workshop.comments;
+
+import io.quarkus.hibernate.orm.panache.PanacheEntity;
+import jakarta.persistence.Entity;
+import jakarta.validation.constraints.NotBlank;
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Entity
+public class Comment extends PanacheEntity {
+    @NotBlank(message="Ref may not be blank")
+    public String ref;
+    public LocalDateTime time;
+    @NotBlank(message="Name may not be blank")
+    public String name;
+    @NotBlank(message="Comment may not be blank")
+    public String comment;
+    
+    public static List<Comment> findRefComments(String ref){
+        return list("ref", ref);
+    }
+}
